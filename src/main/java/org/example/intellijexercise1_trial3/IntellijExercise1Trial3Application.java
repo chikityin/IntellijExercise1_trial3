b0cab134951f30adad59f38c2caaa0cf68f08061
chikityin <>
2024-08-06 14:28:33 -0700

    Project Created

---
+++ src/main/java/org/example/intellijexercise1_trial3/IntellijExercise1Trial3Application.java
@@ -0,0 +1,13 @@
+package org.example.intellijexercise1_trial3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijExercise1Trial3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntellijExercise1Trial3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/intellijexercise1_trial3/IntellijExercise1Trial3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.intellijexercise1_trial3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellijExercise1Trial3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
